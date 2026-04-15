f0bb7d167abe240613b07e7f5a9b12db077f8ef1
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-14 21:00:23 -0400

    Update limbo.js

--- commands/limbo.js
+++ commands/limbo.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { em, wait, hmacFloat, hmacFloat2 } = require('../utils/theme');
-const { log } = require('../utils/logger');
 function roll() {
   const r = hmacFloat();
   if (r < 0.01) return hmacFloat2(50, 100);
@@ -12,12 +11,11 @@ function roll() {
 }
 module.exports = {
   data: new SlashCommandBuilder().setName('limbo').setDescription('🚀  Launch into Limbo'),
-  async execute(interaction, client) {
+  async execute(interaction) {
     await interaction.deferReply();
     await interaction.editReply({ embeds: [em('Konvert Flips\' Limbo', '🚀  Launching...')] });
     await wait(1000);
     const m = roll();
     await interaction.editReply({ embeds: [em('Konvert Flips\' Limbo', '**' + interaction.user.displayName + '** landed on **' + m + 'x**')] });
-    await log(client, { user: interaction.user, game: 'Limbo', result: m >= 2 ? 'WIN' : 'LOSS', detail: m + 'x' });
   },
 };
