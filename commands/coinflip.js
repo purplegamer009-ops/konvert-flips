f2fa7e779325a1de7e38d0a6758a172b1ef60185
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-14 21:00:13 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,14 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { em, wait, hmacRoll } = require('../utils/theme');
-const { log } = require('../utils/logger');
 module.exports = {
   data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙  Flip a coin'),
-  async execute(interaction, client) {
+  async execute(interaction) {
     await interaction.deferReply();
     await interaction.editReply({ embeds: [em('Konvert Flips\' Coinflip', '🪙  Flipping...')] });
     await wait(1000);
     const result = hmacRoll(1, 2) === 1 ? 'HEADS' : 'TAILS';
     await interaction.editReply({ embeds: [em('Konvert Flips\' Coinflip', (result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**')] });
-    await log(client, { user: interaction.user, game: 'Coinflip', result: 'FLIP', detail: result });
   },
 };
