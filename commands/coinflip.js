9c7b01777aae68d326c956a3a5b9f31dcf420751
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-18 15:00:32 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -4,9 +4,9 @@ module.exports = {
   data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙  Flip a coin'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.editReply({ embeds: [em('Konvert Flips\' Coinflip', '🪙  Flipping...')] });
+    await interaction.editReply({ embeds: [em('Konvault\' Coinflip', '🪙  Flipping...')] });
     await wait(1000);
     const result = hmacRoll(1, 2) === 1 ? 'HEADS' : 'TAILS';
-    await interaction.editReply({ embeds: [em('Konvert Flips\' Coinflip', (result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**')] });
+    await interaction.editReply({ embeds: [em('Konvault\' Coinflip', (result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**')] });
   },
 };
