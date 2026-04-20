ac7b0585cae29cfc2a4c7db9b704a5950679258f
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-20 02:53:26 -0400

    Update highcard.js

--- commands/highcard.js
+++ commands/highcard.js
@@ -1,14 +1,34 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { em, wait, hmacRoll } = require('../utils/theme');
+const { em, wait, generateFairRoll } = require('../utils/theme');
+const { storeProof, verifyRow } = require('../utils/verifyButton');
+
 const SUITS=['♠️','♥️','♦️','♣️'];
 const VALS=['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+
 module.exports = {
   data: new SlashCommandBuilder().setName('highcard').setDescription('🃏  Draw a random card'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.editReply({ embeds: [em('Konvault\' High Card','🃏  Drawing...', null, 'highcard')] });
+    await interaction.editReply({ embeds: [em('Konvault\' High Card', '🃏  Drawing...', null, 'highcard')] });
     await wait(1000);
-    const card=VALS[hmacRoll(0,12)]+SUITS[hmacRoll(0,3)];
-    await interaction.editReply({ embeds: [em('Konvault\' High Card','**'+interaction.user.displayName+'** drew **'+card+'**', null, 'highcard')] });
+
+    const valRoll = generateFairRoll(0, 12);
+    const suitRoll = generateFairRoll(0, 3);
+    const card = VALS[valRoll.result] + SUITS[suitRoll.result];
+
+    const proofId = storeProof(interaction.channelId, {
+      id: Date.now() + '_hc',
+      game: 'High Card',
+      result: card,
+      userId: interaction.user.id,
+      serverSeed: valRoll.serverSeed,
+      clientSeed: valRoll.clientSeed,
+      nonce: valRoll.nonce,
+    });
+
+    await interaction.editReply({
+      embeds: [em('Konvault\' High Card', '**' + interaction.user.displayName + '** drew **' + card + '**', null, 'highcard')],
+      components: [verifyRow(proofId)],
+    });
   },
 };
