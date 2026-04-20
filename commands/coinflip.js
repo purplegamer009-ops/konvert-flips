22f860f329c3ceeb56b44c74ea1255a7dd031b9e
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-20 02:57:11 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,38 +1,27 @@
-if (lower === '?cf') {
+const { SlashCommandBuilder } = require('discord.js');
+const { em, wait, generateFairRoll } = require('../utils/theme');
+const { storeProof, verifyRow } = require('../utils/verifyButton');
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙  Flip a coin'),
+  async execute(interaction) {
+    await interaction.deferReply();
+    await interaction.editReply({ embeds: [em('Konvault\' Coinflip', '🪙  Flipping...', null, 'coinflip')] });
+    await wait(1000);
     const roll = generateFairRoll(1, 2);
     const result = roll.result === 1 ? 'HEADS' : 'TAILS';
-    const proofId = storeProof(msg.channelId, {
-      id: Date.now() + '_cf_text',
+    const proofId = storeProof(interaction.channelId, {
+      id: Date.now() + '_cf',
       game: 'Coinflip',
       result,
-      userId: msg.author.id,
+      userId: interaction.user.id,
       serverSeed: roll.serverSeed,
       clientSeed: roll.clientSeed,
       nonce: roll.nonce,
     });
-    await msg.channel.send({
+    await interaction.editReply({
       embeds: [em('Konvault\' Coinflip', (result==='HEADS'?'🟡':'⚪')+'  **'+result+'**', null, 'coinflip')],
       components: [verifyRow(proofId)],
     });
-    return;
-  }
-
-  if (lower === '?dice' || lower === '?roll') {
-    const roll1 = generateFairRoll(1, 6);
-    const roll2 = generateFairRoll(1, 6);
-    const d1 = roll1.result, d2 = roll2.result;
-    const proofId = storeProof(msg.channelId, {
-      id: Date.now() + '_dice_text',
-      game: 'Dice',
-      result: d1 + ' & ' + d2 + ' = ' + (d1+d2),
-      userId: msg.author.id,
-      serverSeed: roll1.serverSeed,
-      clientSeed: roll1.clientSeed,
-      nonce: roll1.nonce,
-    });
-    await msg.channel.send({
-      embeds: [em('Konvault\' Dice Roll', '**'+msg.author.displayName+'** rolled **'+d1+'** & **'+d2+'**\n\nTotal: **'+(d1+d2)+'**', null, 'dice')],
-      components: [verifyRow(proofId)],
-    });
-    return;
-  }
+  },
+};
