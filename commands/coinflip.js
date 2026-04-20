1ae1927dc949374c322561cf9dfd0eebb6636502
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-20 02:52:55 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,12 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { em, wait, hmacRoll } = require('../utils/theme');
+const { em, wait, generateFairRoll } = require('../utils/theme');
+const { storeProof, verifyRow } = require('../utils/verifyButton');
+
 module.exports = {
   data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙  Flip a coin'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.editReply({ embeds: [em('Konvault\' Coinflip','🪙  Flipping...', null, 'coinflip')] });
+    await interaction.editReply({ embeds: [em('Konvault\' Coinflip', '🪙  Flipping...', null, 'coinflip')] });
     await wait(1000);
-    const result = hmacRoll(1,2)===1?'HEADS':'TAILS';
-    await interaction.editReply({ embeds: [em('Konvault\' Coinflip',(result==='HEADS'?'🟡':'⚪')+'  **'+result+'**', null, 'coinflip')] });
+
+    const roll = generateFairRoll(1, 2);
+    const result = roll.result === 1 ? 'HEADS' : 'TAILS';
+
+    const proofId = storeProof(interaction.channelId, {
+      id: Date.now() + '_cf',
+      game: 'Coinflip',
+      result,
+      userId: interaction.user.id,
+      serverSeed: roll.serverSeed,
+      clientSeed: roll.clientSeed,
+      nonce: roll.nonce,
+    });
+
+    await interaction.editReply({
+      embeds: [em('Konvault\' Coinflip', (result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**', null, 'coinflip')],
+      components: [verifyRow(proofId)],
+    });
   },
 };
