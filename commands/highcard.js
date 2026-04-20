ab99d9b385c4bd2835ca733601e70332423bb8ce
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-20 02:17:08 -0400

    Update highcard.js

--- commands/highcard.js
+++ commands/highcard.js
@@ -1,14 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { em, wait, hmacRoll } = require('../utils/theme');
-const SUITS = ['♠️','♥️','♦️','♣️'];
-const VALS = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+const SUITS=['♠️','♥️','♦️','♣️'];
+const VALS=['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 module.exports = {
   data: new SlashCommandBuilder().setName('highcard').setDescription('🃏  Draw a random card'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.editReply({ embeds: [em('Konvault\' High Card', '🃏  Drawing...')] });
+    await interaction.editReply({ embeds: [em('Konvault\' High Card','🃏  Drawing...', null, 'highcard')] });
     await wait(1000);
-    const card = VALS[hmacRoll(0, 12)] + SUITS[hmacRoll(0, 3)];
-    await interaction.editReply({ embeds: [em('Konvault\' High Card', '**' + interaction.user.displayName + '** drew **' + card + '**')] });
+    const card=VALS[hmacRoll(0,12)]+SUITS[hmacRoll(0,3)];
+    await interaction.editReply({ embeds: [em('Konvault\' High Card','**'+interaction.user.displayName+'** drew **'+card+'**', null, 'highcard')] });
   },
 };
