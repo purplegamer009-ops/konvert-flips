b2358524d509e98d798261a5afdbe92c01966433
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-20 02:09:22 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,31 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { em, wait } = require('../utils/theme');
-const { log } = require('../utils/logger');
-
+const { em, wait, hmacRoll } = require('../utils/theme');
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('coinflip')
-    .setDescription('🪙  Flip a coin'),
-
-  async execute(interaction, client) {
+  data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙  Flip a coin'),
+  async execute(interaction) {
     await interaction.deferReply();
-
-    await interaction.editReply({ embeds: [em('Konvert Flips\' Coinflip', '🪙  Flipping...')] });
+    await interaction.editReply({ embeds: [em('Konvault\' Coinflip','🪙  Flipping...', null, 'coinflip')] });
     await wait(1000);
-
-    const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
-    const emoji  = result === 'Heads' ? '🟡' : '⚪';
-
-    await interaction.editReply({ embeds: [em(
-      'Konvert Flips\' Coinflip',
-      `${emoji}  **${result}**`
-    )] });
-
-    await log(client, {
-      user: interaction.user,
-      game: 'Coinflip',
-      result: 'FLIP',
-      detail: result,
-    });
+    const result = hmacRoll(1,2)===1?'HEADS':'TAILS';
+    await interaction.editReply({ embeds: [em('Konvault\' Coinflip',(result==='HEADS'?'🟡':'⚪')+'  **'+result+'**', null, 'coinflip')] });
   },
 };
