9761f10163b3c583afe26df16f61372461a9f0a8
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-25 23:44:35 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,16 +1,26 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { wait, generateFairRoll, IMAGES, PURPLE } = require('../utils/theme');
-const { storeProof, verifyFooter } = require('../utils/verifyButton');
+const { storeProof, verifyRow } = require('../utils/verifyButton');
 module.exports = {
   data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙 Flip a coin'),
   async execute(interaction) {
     await interaction.deferReply();
+    await interaction.editReply({ embeds: [new EmbedBuilder().setColor(PURPLE).setDescription('🪙  Flipping...').setThumbnail(IMAGES.coinflip).setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })] });
+    await wait(900);
     const roll = generateFairRoll(1, 2);
     const result = roll.result === 1 ? 'HEADS' : 'TAILS';
-    await wait(800);
     const proofId = storeProof(interaction.channelId, { id: Date.now() + '_cf', game: 'Coinflip', result, userId: interaction.user.id, serverSeed: roll.serverSeed, clientSeed: roll.clientSeed, nonce: roll.nonce });
-    await interaction.editReply({ embeds: [new EmbedBuilder().setColor(PURPLE)
-      .setDescription((result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**' + verifyFooter(proofId))
-      .setThumbnail(IMAGES.coinflip).setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })] });
+    await interaction.editReply({
+      embeds: [new EmbedBuilder()
+        .setColor(PURPLE)
+        .setTitle('🪙  Coin Flip')
+        .setThumbnail(IMAGES.coinflip)
+        .setDescription((result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**')
+        .addFields({ name: 'Player', value: interaction.user.displayName, inline: true }, { name: 'Result', value: result, inline: true })
+        .setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })
+        .setTimestamp()
+      ],
+      components: [verifyRow(proofId)],
+    });
   },
 };
