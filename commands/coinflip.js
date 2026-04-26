b8436571d964a0d85291b19260e55059883bc890
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-25 23:20:21 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,27 +1,16 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { em, wait, generateFairRoll } = require('../utils/theme');
-const { storeProof, verifyRow } = require('../utils/verifyButton');
-
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { wait, generateFairRoll, IMAGES, PURPLE } = require('../utils/theme');
+const { storeProof, verifyFooter } = require('../utils/verifyButton');
 module.exports = {
-  data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙  Flip a coin'),
+  data: new SlashCommandBuilder().setName('coinflip').setDescription('🪙 Flip a coin'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.editReply({ embeds: [em('Konvault\' Coinflip', '🪙  Flipping...', null, 'coinflip')] });
-    await wait(1000);
     const roll = generateFairRoll(1, 2);
     const result = roll.result === 1 ? 'HEADS' : 'TAILS';
-    const proofId = storeProof(interaction.channelId, {
-      id: Date.now() + '_cf',
-      game: 'Coinflip',
-      result,
-      userId: interaction.user.id,
-      serverSeed: roll.serverSeed,
-      clientSeed: roll.clientSeed,
-      nonce: roll.nonce,
-    });
-    await interaction.editReply({
-      embeds: [em('Konvault\' Coinflip', (result==='HEADS'?'🟡':'⚪')+'  **'+result+'**', null, 'coinflip')],
-      components: [verifyRow(proofId)],
-    });
+    await wait(800);
+    const proofId = storeProof(interaction.channelId, { id: Date.now() + '_cf', game: 'Coinflip', result, userId: interaction.user.id, serverSeed: roll.serverSeed, clientSeed: roll.clientSeed, nonce: roll.nonce });
+    await interaction.editReply({ embeds: [new EmbedBuilder().setColor(PURPLE)
+      .setDescription((result === 'HEADS' ? '🟡' : '⚪') + '  **' + result + '**' + verifyFooter(proofId))
+      .setThumbnail(IMAGES.coinflip).setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })] });
   },
 };
