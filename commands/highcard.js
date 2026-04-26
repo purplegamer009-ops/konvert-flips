c5611e63115b9b16579628eadab0bac235300876
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-25 23:21:04 -0400

    Update highcard.js

--- commands/highcard.js
+++ commands/highcard.js
@@ -1,34 +1,17 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { em, wait, generateFairRoll } = require('../utils/theme');
-const { storeProof, verifyRow } = require('../utils/verifyButton');
-
-const SUITS=['♠️','♥️','♦️','♣️'];
-const VALS=['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { wait, generateFairRoll, IMAGES, PURPLE } = require('../utils/theme');
+const { storeProof, verifyFooter } = require('../utils/verifyButton');
+const SUITS=['♠️','♥️','♦️','♣️'],VALS=['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 module.exports = {
-  data: new SlashCommandBuilder().setName('highcard').setDescription('🃏  Draw a random card'),
+  data: new SlashCommandBuilder().setName('highcard').setDescription('🃏 Draw a card'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.editReply({ embeds: [em('Konvault\' High Card', '🃏  Drawing...', null, 'highcard')] });
-    await wait(1000);
-
-    const valRoll = generateFairRoll(0, 12);
-    const suitRoll = generateFairRoll(0, 3);
-    const card = VALS[valRoll.result] + SUITS[suitRoll.result];
-
-    const proofId = storeProof(interaction.channelId, {
-      id: Date.now() + '_hc',
-      game: 'High Card',
-      result: card,
-      userId: interaction.user.id,
-      serverSeed: valRoll.serverSeed,
-      clientSeed: valRoll.clientSeed,
-      nonce: valRoll.nonce,
-    });
-
-    await interaction.editReply({
-      embeds: [em('Konvault\' High Card', '**' + interaction.user.displayName + '** drew **' + card + '**', null, 'highcard')],
-      components: [verifyRow(proofId)],
-    });
+    const vr=generateFairRoll(0,12),sr=generateFairRoll(0,3);
+    const card=VALS[vr.result]+SUITS[sr.result];
+    await wait(800);
+    const proofId=storeProof(interaction.channelId,{id:Date.now()+'_hc',game:'High Card',result:card,userId:interaction.user.id,serverSeed:vr.serverSeed,clientSeed:vr.clientSeed,nonce:vr.nonce});
+    await interaction.editReply({ embeds: [new EmbedBuilder().setColor(PURPLE)
+      .setDescription('🃏 **' + interaction.user.displayName + '** drew **' + card + '**' + verifyFooter(proofId))
+      .setThumbnail(IMAGES.highcard).setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })] });
   },
 };
