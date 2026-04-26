54d1c436b9bf24af4064c359ea8a6a4f4d540d7c
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-25 23:44:43 -0400

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -1,17 +1,31 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { wait, generateFairRoll, IMAGES, PURPLE } = require('../utils/theme');
-const { storeProof, verifyFooter } = require('../utils/verifyButton');
+const { storeProof, verifyRow } = require('../utils/verifyButton');
 module.exports = {
   data: new SlashCommandBuilder().setName('dice').setDescription('🎲 Roll two dice')
     .addIntegerOption(o => o.setName('sides').setDescription('Sides (default 6)').setMinValue(2).setMaxValue(100)),
   async execute(interaction) {
     await interaction.deferReply();
+    await interaction.editReply({ embeds: [new EmbedBuilder().setColor(PURPLE).setDescription('🎲  Rolling...').setThumbnail(IMAGES.dice).setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })] });
+    await wait(900);
     const sides = interaction.options.getInteger('sides') ?? 6;
     const r1 = generateFairRoll(1, sides), r2 = generateFairRoll(1, sides);
-    await wait(800);
     const proofId = storeProof(interaction.channelId, { id: Date.now() + '_dice', game: 'Dice', result: r1.result + ' & ' + r2.result, userId: interaction.user.id, serverSeed: r1.serverSeed, clientSeed: r1.clientSeed, nonce: r1.nonce });
-    await interaction.editReply({ embeds: [new EmbedBuilder().setColor(PURPLE)
-      .setDescription('**' + interaction.user.displayName + '** rolled **' + r1.result + '** & **' + r2.result + '** — total **' + (r1.result + r2.result) + '**' + verifyFooter(proofId))
-      .setThumbnail(IMAGES.dice).setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })] });
+    await interaction.editReply({
+      embeds: [new EmbedBuilder()
+        .setColor(PURPLE)
+        .setTitle('🎲  Dice Roll')
+        .setThumbnail(IMAGES.dice)
+        .setDescription('**' + interaction.user.displayName + '** rolled the dice')
+        .addFields(
+          { name: 'Die 1', value: '**' + r1.result + '**', inline: true },
+          { name: 'Die 2', value: '**' + r2.result + '**', inline: true },
+          { name: 'Total', value: '**' + (r1.result + r2.result) + '**', inline: true },
+        )
+        .setFooter({ text: 'KONVAULT™', iconURL: IMAGES.logo })
+        .setTimestamp()
+      ],
+      components: [verifyRow(proofId)],
+    });
   },
 };
