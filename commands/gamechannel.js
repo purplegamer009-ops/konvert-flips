485f9724ec789868979e3be196fadfb2aacbb6b9
purplegamer009-ops <purplegamer009@gmail.com>
2026-04-25 23:27:03 -0400

    Update gamechannel.js

--- commands/gamechannel.js
+++ commands/gamechannel.js
@@ -1,15 +1,13 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { em } = require('../utils/theme');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { IMAGES, PURPLE } = require('../utils/theme');
 module.exports = {
-  data: new SlashCommandBuilder().setName('gamechannel').setDescription('🎮  Owner: lock channel to 2 players only')
+  data: new SlashCommandBuilder().setName('gamechannel').setDescription('🎮 Owner: lock channel to 2 players only')
     .addUserOption(o=>o.setName('player1').setDescription('First player').setRequired(true))
     .addUserOption(o=>o.setName('player2').setDescription('Second player').setRequired(true)),
   async execute(interaction,client){
-    if(interaction.user.id!==process.env.OWNER_ID)return interaction.reply({content:'🚫  Owner only.',ephemeral:true});
-    const p1=interaction.options.getUser('player1');
-    const p2=interaction.options.getUser('player2');
-    const channel=interaction.channel;
-    const guild=interaction.guild;
+    if(interaction.user.id!==process.env.OWNER_ID)return interaction.reply({content:'🚫 Owner only.',ephemeral:true});
+    const p1=interaction.options.getUser('player1'),p2=interaction.options.getUser('player2');
+    const channel=interaction.channel,guild=interaction.guild;
     await interaction.deferReply();
     try{
       await channel.permissionOverwrites.edit(guild.roles.everyone,{SendMessages:false});
@@ -17,7 +15,7 @@ module.exports = {
       await channel.permissionOverwrites.edit(p2.id,{SendMessages:true,ViewChannel:true});
       await channel.permissionOverwrites.edit(interaction.user.id,{SendMessages:true,ViewChannel:true});
       await channel.permissionOverwrites.edit(client.user.id,{SendMessages:true,ViewChannel:true,EmbedLinks:true});
-      await interaction.editReply({embeds:[em('Konvault\' Game Channel','🎮  Channel locked to 2 players\n\n👤  <@'+p1.id+'>\n👤  <@'+p2.id+'>\n\nEveryone else can watch.\nUse `/endgame` to unlock.')]});
-    }catch(err){console.error(err);await interaction.editReply({content:'❌  Failed. Make sure bot has Manage Channel permission.',ephemeral:true});}
+      await interaction.editReply({embeds:[new EmbedBuilder().setColor(PURPLE).setDescription('🎮 Channel locked to <@'+p1.id+'> and <@'+p2.id+'>\n\nEveryone else can watch. Use `/endgame` to unlock.').setFooter({text:'KONVAULT™',iconURL:IMAGES.logo})]});
+    }catch(e){await interaction.editReply({content:'❌ Failed. Make sure bot has Manage Channel permission.'});}
   },
 };
